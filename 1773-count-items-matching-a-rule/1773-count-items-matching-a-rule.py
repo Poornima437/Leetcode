3203ced9f989f3eed8cec8fa67b298844cc902d1
Poornima437 <npoornima2005@gmail.com>
2025-03-08 10:07:41 +0530

    Time: 8 ms (14.29%), Space: 16.7 MB (49.62%) - LeetHub

---
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def countMatches(self, items, ruleKey, ruleValue):
+
+        count=0
+        for i in items:
+            if ruleKey=="type" and ruleValue==i[0]:
+                count+=1
+            elif ruleKey=="color" and ruleValue==i[1]:
+                count+=1
+            elif ruleKey=="name" and ruleValue==i[2]:
+                count+=1
+        return count
+        
