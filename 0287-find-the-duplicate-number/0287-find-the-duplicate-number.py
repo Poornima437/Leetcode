0456ea943d4bfd26cb9314aac5c2aa0840793182
Poornima437 <npoornima2005@gmail.com>
2025-05-02 09:14:47 +0530

    Time: 26 ms (88.96%), Space: 27.2 MB (12%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def findDuplicate(self, nums):
+        dupli=set()
+        for i in nums:
+            if i in dupli:
+                return i
+            dupli.add(i)
+        
