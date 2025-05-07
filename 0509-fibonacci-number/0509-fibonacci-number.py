439c661213ba6a74d5ad9048a77545ba92391650
Poornima437 <npoornima2005@gmail.com>
2025-05-07 09:52:36 +0530

    Time: 7 ms (98.55%), Space: 12.4 MB (50.67%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,11 +1,12 @@
 class Solution(object):
     def fib(self, n):
-        if n==0:
-            return 0
-        elif n==1:
-            return 1
-        a,b=0,1
-        for i in range(2,n+1):
-            a,b=b, a+b
-        return b
+        a=0
+        b=1
+        for i in range(1,n+1):
+            a,b=b,a+b
+        return a
+        """
+        :type n: int
+        :rtype: int
+        """
         
